ee1f31134203fc3eb77c832706bd81bea1a2fcef
Maxime Lamothe-Brassard <maxime@refractionpoint.com>
2019-04-30 18:46:27 -0500

    Adding basic support for multiplexing Replay queries.

--- limacharlie/BatchDR.py
+++ limacharlie/BatchDR.py
@@ -1,3 +1,5 @@
+from gevent import monkey; monkey.patch_all()
+from gevent.lock import BoundedSemaphore
 from . import Manager
 from .utils import LcApiException
 
@@ -8,41 +10,147 @@ import urllib2
 import base64
 import json
 import yaml
+import gevent
+import time
+import sys
 
 class BatchDR( object ):
-    def __init__( self, manager ):
+    def __init__( self, manager, maxTimeWindow = ( 60 * 60 * 24 * 1 ), maxConcurrent = 10, isInteractive = False ):
         self._lc = manager
         self._apiURL = None
+        self._maxTimeWindow = maxTimeWindow
+        self._maxConcurrent = maxConcurrent
+        self._replayURL = self._lc.getOrgURLs()[ 'replay' ]
 
+        self._isInteracive = isInteractive
+        self._statusMutex = BoundedSemaphore()
+        self._queryPending = 0
+        self._sensorPending = 0
+        if self._isInteracive:
+            gevent.spawn_later( 0, self._reportStatus )
+
+    def _reportStatus( self ):
+        with self._statusMutex:
+            sys.stdout.write( "\rSensors pending: %8s, queries pending: %8s" % ( self._sensorPending, self._queryPending ) )
+        sys.stdout.flush()
+
+        gevent.spawn_later( 1, self._reportStatus )
 
     def scanHistoricalSensor( self, sid, startTime, endTime, ruleName = None, ruleContent = None ):
-        if self._apiURL is None:
-            # Get the ingest URL from the API.
-            self._apiURL = 'https://%s/' % ( self._lc.getOrgURLs()[ 'replay' ], )
-        req = {
-            'start' : startTime,
-            'end' : endTime,
-        }
-        body = None
-        if ruleName is not None:
-            req[ 'rule_name' ] = ruleName
-        elif ruleContent is not None:
-            body = json.dumps( ruleContent )
-        else:
-            raise LcApiException( 'no rule specified' )
+        windows = []
 
-        statusCode, resp = self._lc._restCall( 'sensor/%s/%s' % ( self._lc._oid, sid, ), 
-                                               'POST', 
-                                               {}, 
-                                               altRoot = self._apiURL,
-                                               queryParams = req,
-                                               rawBody = body,
-                                               contentType = 'application/json' )
+        with self._statusMutex:
+            if 0 == self._sensorPending:
+                self._sensorPending = 1
 
-        if 200 != statusCode:
-            raise LcApiException( '%s: %s' % ( statusCode, resp ) )
+        try:
+            # Split up the total time into windows we can query in parallel.
+            if endTime - startTime > self._maxTimeWindow:
+                tmpStart = startTime
+                while tmpStart < endTime:
+                    tmpEnd = min( tmpStart + self._maxTimeWindow, endTime )
+                    windows.append( ( tmpStart, tmpEnd ) )
+                    tmpStart += self._maxTimeWindow
+            else:
+                windows = [ ( startTime, endTime ) ]
+
+            with self._statusMutex:
+                self._queryPending += len( windows )
+
+            results = self._parallelExec( lambda w: self._scanHistoricalSensor( sid, w[ 0 ], w[ 1 ], ruleName = ruleName, ruleContent = ruleContent ), windows, maxConcurrent = self._maxConcurrent )
+        finally:
+            with self._statusMutex:
+                self._sensorPending -= 1
+
+        return self._rollupResults( results )
+
+    def _scanHistoricalSensor( self, sid, startTime, endTime, ruleName = None, ruleContent = None ):
+        # print( "Starting query %s-%s for %s" % ( startTime, endTime, sid ) )
+        qStart = time.time()
+
+        try:
+            if self._apiURL is None:
+                # Get the ingest URL from the API.
+                self._apiURL = 'https://%s/' % ( self._replayURL, )
+            req = {
+                'start' : startTime,
+                'end' : endTime,
+            }
+            body = None
+            if ruleName is not None:
+                req[ 'rule_name' ] = ruleName
+            elif ruleContent is not None:
+                body = json.dumps( ruleContent )
+            else:
+                raise LcApiException( 'no rule specified' )
+
+            statusCode, resp = self._lc._restCall( 'sensor/%s/%s' % ( self._lc._oid, sid, ),
+                                                  'POST',
+                                                  {},
+                                                  altRoot = self._apiURL,
+                                                  queryParams = req,
+                                                  rawBody = body,
+                                                  contentType = 'application/json' )
+
+            if 200 != statusCode:
+                raise LcApiException( '%s: %s' % ( statusCode, resp ) )
+        finally:
+            with self._statusMutex:
+                self._queryPending -= 1
+        # print( "Finished query %s-%s for %s in %s seconds" % ( startTime, endTime, sid, time.time() - qStart ) )
 
         return resp
+
+    def scanEntireOrg( self, startTime, endTime, ruleName = None, ruleContent = None ):
+        sensors = self._lc.sensors()
+        while True:
+            moreSensors = self._lc.sensors( is_next = True )
+            if moreSensors is None:
+                break
+            sensors += moreSensors
+
+        with self._statusMutex:
+            self._sensorPending = len( sensors )
+
+        results = self._parallelExec( lambda sid: self.scanHistoricalSensor( sid, startTime, endTime, ruleName = ruleName, ruleContent = ruleContent ), sensors, maxConcurrent = self._maxConcurrent )
+
+        return self._rollupResults( results )
+
+    def _rollupResults( self, results ):
+        final = {}
+        for result in results:
+            if not isinstance( result, dict ):
+                raise result
+            for k, v in result.iteritems():
+                if isinstance( v, ( int, float ) ):
+                    if k not in final:
+                        final[ k ] = 0
+                    final[ k ] += v
+                elif isinstance( v, ( str, unicode, bytes ) ):
+                    final.setdefault( k, [] ).append( v )
+                elif isinstance( v, dict ):
+                    final.setdefault( k, {} ).update( v )
+                elif isinstance( v, ( list, tuple ) ):
+                    tmp = final.setdefault( k, [] )
+                    tmp += list( v )
+                else:
+                    raise LcApiException( 'unexpected data type: %s' % ( type( v ), ) )
+        return final
+
+    def _parallelExec( self, f, objects, timeout = None, maxConcurrent = None ):
+        g = gevent.pool.Pool( size = maxConcurrent )
+        results = g.imap_unordered( lambda o: self._retExecOrExc( f, o, timeout ), objects )
+        return list( results )
+
+    def _retExecOrExc( self, f, o, timeout ):
+        try:
+            if timeout is None:
+                return f( o )
+            else:
+                with gevent.Timeout( timeout ):
+                    return f( o )
+        except ( Exception, gevent.Timeout ) as e:
+            return e
 
 def main():
     import argparse
@@ -52,10 +160,17 @@ def main():
 
     parser.add_argument( '--sid',
                          type = uuid.UUID,
-                         required = True,
+                         required = False,
                          dest = 'sid',
                          default = None,
                          help = 'sensor id to scan traffic from.' )
+
+    parser.add_argument( '--entire-org',
+                         action = 'store_true',
+                         default = False,
+                         required = False,
+                         dest = 'isEntireOrg',
+                         help = 'if set and --sid is not set, replay traffic from entire organization.' )
 
     parser.add_argument( '--start',
                          type = int,
@@ -87,7 +202,7 @@ def main():
 
     args = parser.parse_args()
 
-    batchDr = BatchDR( Manager( None, None ) )
+    batchDr = BatchDR( Manager( None, None ), isInteractive = True )
 
     ruleContent = None
     if args.ruleContent is not None:
@@ -101,10 +216,18 @@ def main():
             except:
                 raise LcApiException( 'rule content not valid yaml or json' )
 
-    response = batchDr.scanHistoricalSensor( str( args.sid ), 
-                                             args.start, 
-                                             args.end,
-                                             ruleName = args.ruleName,
-                                             ruleContent = ruleContent )
+    if args.sid is not None:
+        response = batchDr.scanHistoricalSensor( str( args.sid ),
+                                                args.start,
+                                                args.end,
+                                                ruleName = args.ruleName,
+                                                ruleContent = ruleContent )
+    elif args.isEntireOrg:
+        response = batchDr.scanEntireOrg( args.start,
+                                          args.end,
+                                          ruleName = args.ruleName,
+                                          ruleContent = ruleContent )
+    else:
+        raise LcApiException( '--sid or --entire-org must be specified' )
 
-    print( json.dumps( response ) )
+    print( json.dumps( response, indent = 2 ) )
