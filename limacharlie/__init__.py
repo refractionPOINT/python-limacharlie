158764abf1fc036716936917c9c1316b58f0c19e
Maxime Lamothe-Brassard <maxime@refractionpoint.com>
2019-05-20 10:40:20 -0500

    Making uid optional in config.

--- limacharlie/__init__.py
+++ limacharlie/__init__.py
@@ -35,7 +35,7 @@ if GLOBAL_API_KEY is None:
                 if _credsFile.get( 'env', {} ).get( _lcEnv, None ) is None:
                     raise Exception( "LimaCharlie environment specified in LC_CURRENT_ENV could not be found in local config file: ~/.limacharlie" )
                 GLOBAL_OID = _credsFile[ 'env' ][ _lcEnv ][ 'oid' ]
-                GLOBAL_UID = _credsFile[ 'env' ][ _lcEnv ][ 'uid' ]
+                GLOBAL_UID = _credsFile[ 'env' ][ _lcEnv ].get( 'uid', None )
                 GLOBAL_API_KEY = _credsFile[ 'env' ][ _lcEnv ][ 'api_key' ]
 
 from .Manager import Manager
