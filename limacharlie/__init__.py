b414beb77fa40221c9c597590f60ece7a5f2ebe5
Maxime Lamothe-Brassard <maxime@refractionpoint.com>
2019-07-30 17:10:18 -0500

    Version bump.

--- limacharlie/__init__.py
+++ limacharlie/__init__.py
@@ -1,6 +1,6 @@
 """limacharlie API for limacharlie.io"""
 
-__version__ = "2.14.4"
+__version__ = "2.15.0"
 __author__ = "Maxime Lamothe-Brassard ( Refraction Point, Inc )"
 __author_email__ = "maxime@refractionpoint.com"
 __license__ = "Apache v2"
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-__version__ = "2.14.4"
+__version__ = "2.15.0"
 __author__ = "Maxime Lamothe-Brassard ( Refraction Point, Inc )"
 __author_email__ = "maxime@refractionpoint.com"
 __license__ = "Apache v2"
