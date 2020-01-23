41755e50ce5276426526d8609d689fa48bc147e5
Maxime Lamothe-Brassard <maxime@refractionpoint.com>
2020-01-22 16:06:45 -0800

    Preventing monkey patching warnings from stopping execution.

--- limacharlie/Spout.py
+++ limacharlie/Spout.py
@@ -1,5 +1,10 @@
-from gevent import monkey
-monkey.patch_all()
+try:
+    from gevent import monkey
+    monkey.patch_all()
+except monkey.MonkeyPatchWarning as e:
+    import sys
+    sys.stderr.write( "%s\n" % ( e, ) )
+    sys.stderr.flush()
 from gevent.queue import Queue
 import gevent.pool
 import sys
