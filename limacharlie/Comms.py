42baba1812b2ce17c6e4bfeda6c18e6a08ce0010
Maxime Lamothe-Brassard <maxime@refractionpoint.com>
2020-11-23 12:07:16 -0800

    Adding more accessors.

--- limacharlie/Comms.py
+++ limacharlie/Comms.py
@@ -44,6 +44,17 @@ class Comms( object ):
         '''
         return Room( self._manager, rid )
 
+    def search( self, filters = {} ):
+        '''Search for matching Rooms across all available Orgs.
+
+        Args:
+            filters (dict): dictionary of all the filter terms,
+        Returns:
+            A list of matching Rooms.
+        '''
+        filters = { k : v for k, v in filters.items() if v is not None }
+        return self._manager._apiCall( 'comms/search', GET, queryParams = filters, altRoot = ROOT_URL )
+
 class Room( object ):
     '''Representation of a limacharlie.io Comms Room.'''
 
@@ -101,7 +112,31 @@ class Room( object ):
             req[ 'priority' ] = int( priority )
         if status is not None:
             req[ 'status' ] = str( status )
-        return self._manager._apiCall( 'comms/room/%s' % self.rid, POST, altRoot = ROOT_URL )
+        return self._manager._apiCall( 'comms/room/%s' % self.rid, POST, req, altRoot = ROOT_URL )
+
+    def addLink( self, linkType, linkValue ):
+        '''Add a link to a Room.
+
+        Args:
+            link (str): link to add.
+        '''
+        req = {
+            'type' : linkType,
+            'value' : linkValue,
+        }
+        return self._manager._apiCall( 'comms/room/%s/link' % self.rid, POST, req, altRoot = ROOT_URL )
+
+    def removeLink( self, linkType, linkValue ):
+        '''Remove a link from a Room.
+
+        Args:
+            link (str): link to remove.
+        '''
+        req = {
+            'type' : linkType,
+            'value' : linkValue,
+        }
+        return self._manager._apiCall( 'comms/room/%s/link' % self.rid, DELETE, req, altRoot = ROOT_URL )
 
 def main( sourceArgs = None ):
     import argparse
@@ -124,6 +159,31 @@ def main( sourceArgs = None ):
     parser_room_get = subparsers_room.add_parser( 'get', help = 'get a room' )
     parser_room_get.add_argument( 'rid', type = str, help = 'room id' )
 
+    # room:search
+    parser_room_search = subparsers_room.add_parser( 'search', help = 'search for rooms' )
+    parser_room_search.add_argument( '--oid', type = str, help = 'on search org oid' )
+    parser_room_search.add_argument( '--cursor', type = str, help = 'search cursor' )
+    parser_room_search.add_argument( '--assignee', type = str, help = 'has assignee' )
+    parser_room_search.add_argument( '--last-change-before', type = str, help = 'last changed before' )
+    parser_room_search.add_argument( '--last-change-after', type = str, help = 'last changed after' )
+    parser_room_search.add_argument( '--created-by', type = str, help = 'created-by' )
+    parser_room_search.add_argument( '--status', type = str, help = 'status' )
+    parser_room_search.add_argument( '--priority-above', type = str, help = 'priority is above' )
+    parser_room_search.add_argument( '--priority-below', type = str, help = 'priority is below' )
+    parser_room_search.add_argument( '--link', type = str, nargs = '*', help = 'link' )
+
+    # room:add_link
+    parser_room_add_link = subparsers_room.add_parser( 'add-link', help = 'add a link to a room' )
+    parser_room_add_link.add_argument( 'rid', type = str, help = 'room id' )
+    parser_room_add_link.add_argument( 'linkType', type = str, help = 'link type' )
+    parser_room_add_link.add_argument( 'linkValue', type = str, help = 'link value' )
+
+    # room:remove_link
+    parser_room_remove_link = subparsers_room.add_parser( 'remove-link', help = 'remove a link from a room' )
+    parser_room_remove_link.add_argument( 'rid', type = str, help = 'room id' )
+    parser_room_remove_link.add_argument( 'linkType', type = str, help = 'link value' )
+    parser_room_remove_link.add_argument( 'linkValue', type = str, help = 'link value' )
+
     args = parser.parse_args( sourceArgs )
 
     if args.object is None:
@@ -141,9 +201,21 @@ def main( sourceArgs = None ):
     def getRoom():
         return Comms( Manager() ).getRoom( args.rid ).getDetails()
 
+    def searchRooms():
+        return Comms( Manager() ).search( filters = vars( args ) )
+
+    def addRoomLink():
+        return Comms( Manager() ).getRoom( args.rid ).addLink( args.linkType, args.linkValue )
+
+    def removeRoomLink():
+        return Comms( Manager() ).getRoom( args.rid ).removeLink( args.linkType, args.linkValue )
+
     result = {
         'room:create' : createRoom,
         'room:get' : getRoom,
+        'room:search' : searchRooms,
+        'room:add-link' : addRoomLink,
+        'room:remove-link' : removeRoomLink,
     }[ '%s:%s' % ( args.object, args.action ) ]()
 
     print( json.dumps( result, indent = 2 ) )
