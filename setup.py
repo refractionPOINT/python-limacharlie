9ba5b0bcfdb068cb46045cbe2cb1d04837e4fea1
Maxime Lamothe-Brassard <maxime@refractionpoint.com>
2021-01-20 16:57:35 -0800

    Version bump

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-__version__ = "3.16.2"
+__version__ = "3.17.0"
 __author__ = "Maxime Lamothe-Brassard ( Refraction Point, Inc )"
 __author_email__ = "maxime@refractionpoint.com"
 __license__ = "Apache v2"
