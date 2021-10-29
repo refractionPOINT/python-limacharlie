4170cce865f6c3e6de37e18f84eed97c389348a1
Maxime Lamothe-Brassard <maxime@refractionpoint.com>
2021-10-28 20:11:53 -0700

    Bump version and fix loader issue with exfil.

--- limacharlie/__init__.py
+++ limacharlie/__init__.py
@@ -1,6 +1,6 @@
 """limacharlie API for limacharlie.io"""
 
-__version__ = "3.20.0"
+__version__ = "3.20.1"
 __author__ = "Maxime Lamothe-Brassard ( Refraction Point, Inc )"
 __author_email__ = "maxime@refractionpoint.com"
 __license__ = "Apache v2"
