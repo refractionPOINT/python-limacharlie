998730bbecfd52edbd242be1a1cac24f7b970759
Maxime Lamothe-Brassard <maxime@refractionpoint.com>
2022-09-29 09:18:09 -0700

    Refactor the CLI to use the object.

--- limacharlie/Hive.py
+++ limacharlie/Hive.py
@@ -29,18 +29,21 @@ def reportError( msg ):
     sys.exit( 1 )
 
 class Hive( object ):
-    def __init__( self, man, hiveName ):
+    def __init__( self, man, hiveName, altPartitionKey = None ):
         self._hiveName = hiveName
         self._man = man
+        self._partitionKey = altPartitionKey
+        if self._partitionKey is None:
+            self._partitionKey = self._man._oid
 
     def list( self ):
-        return { recordName : HiveRecord( recordName, record, self ) for recordName, record in self._man._apiCall( 'hive/%s/%s' % ( self._hiveName, self._man._oid ), GET ).items() }
+        return { recordName : HiveRecord( recordName, record, self ) for recordName, record in self._man._apiCall( 'hive/%s/%s' % ( self._hiveName, self._partitionKey ), GET ).items() }
 
     def get( self, recordName ):
-        return HiveRecord( recordName, self._man._apiCall( 'hive/%s/%s/%s/data' % ( self._hiveName, self._man._oid, urlescape( recordName ) ), GET ), self )
+        return HiveRecord( recordName, self._man._apiCall( 'hive/%s/%s/%s/data' % ( self._hiveName, self._partitionKey, urlescape( recordName ) ), GET ), self )
 
     def getMetadata( self, recordName ):
-        return HiveRecord( recordName, self._man._apiCall( 'hive/%s/%s/%s/mtd' % ( self._hiveName, self._man._oid, urlescape( recordName ) ), GET ), self )
+        return HiveRecord( recordName, self._man._apiCall( 'hive/%s/%s/%s/mtd' % ( self._hiveName, self._partitionKey, urlescape( recordName ) ), GET ), self )
 
     def set( self, record ):
         target = 'mtd'
@@ -66,10 +69,10 @@ class Hive( object ):
         if len( usrMtd ) != 0:
             req[ 'usr_mtd' ] = json.dumps( usrMtd )
 
-        return self._man._apiCall( 'hive/%s/%s/%s/%s' % ( self._hiveName, self._man._oid, urlescape( record.name ), target ), POST, req )
+        return self._man._apiCall( 'hive/%s/%s/%s/%s' % ( self._hiveName, self._partitionKey, urlescape( record.name ), target ), POST, req )
 
     def delete( self, recordName ):
-        return self._man._apiCall( 'hive/%s/%s/%s' % ( self._hiveName, self._man._oid, urlescape( recordName ) ), DELETE )
+        return self._man._apiCall( 'hive/%s/%s/%s' % ( self._hiveName, self._partitionKey, urlescape( recordName ) ), DELETE )
 
 class HiveRecord( object ):
     def __init__( self, recordName, data, api = None ):
@@ -90,6 +93,25 @@ class HiveRecord( object ):
         self.lastError = data.get( 'sys_mtd', {} ).get( 'last_error', None )
         self.lastErrorTime = data.get( 'sys_mtd', {} ).get( 'last_error_ts', None )
 
+    def toJSON( self ):
+        return {
+            'data' : self.data,
+            'usr_mtd' : {
+                'expiry' : self.expiry,
+                'enabled' : self.enabled,
+                'tags' : self.tags,
+            },
+            'sys_mtd' : {
+                'created_at' : self.createdAt,
+                'created_by' : self.createdBy,
+                'guid' : self.guid,
+                'last_author' : self.lastAuthor,
+                'last_mod' : self.lastModified,
+                'last_error' : self.lastError,
+                'last_error_ts' : self.lastErrorTime,
+            },
+        }
+
     def delete( self ):
         return self._api.delete( self.name )
 
@@ -97,6 +119,14 @@ class HiveRecord( object ):
         return self._api.get( self.name )
 
     def update( self, cb = None ):
+        # Perform a transactional update of the record
+        # by using the "etag" provided by the API to make
+        # sure we don't overwrite changes made to the
+        # record between a fetch and a set.
+        # The cb function will get called with the record
+        # and expects a modified record to be returned by
+        # it. This may get called more than once with updated
+        # records if the transaction hits changes.
         if cb is None:
             cb = lambda x: x
         record = self
@@ -112,49 +142,39 @@ class HiveRecord( object ):
                 return ret
             record = self.fetch()
 
-def do_list( args, man, isPrint = True ):
-    resp = man._apiCall( 'hive/%s/%s' % ( args.hive_name, args.partitionKey ), GET )
-    if isPrint:
-        printData( resp )
-    return resp
+def _do_list( args, man ):
+    printData( { r.name: r.toJSON() for r in Hive( man, args.hive_name, altPartitionKey = args.partitionKey ).list().values() } )
 
-def do_list_mtd( args, man, isPrint = True ):
-    resp = man._apiCall( 'hive/%s/%s' % ( args.hive_name, args.partitionKey ), GET )
-    if isPrint:
-        for k, r in resp.items():
-            r[ 'data' ] = None
-        printData( resp )
-    return resp
+def _do_list_mtd( args, man ):
+    resp = { r.name: r.toJSON() for r in Hive( man, args.hive_name, altPartitionKey = args.partitionKey ).list().values() }
+    for k, r in resp.items():
+        r[ 'data' ] = None
+    printData( resp )
 
-def do_get( args, man, isPrint = True ):
+def _do_get( args, man ):
     if args.key is None:
         reportError( 'Key required' )
 
-    resp = man._apiCall( 'hive/%s/%s/%s/data' % ( args.hive_name, args.partitionKey, urlescape( args.key ) ), GET )
-    if isPrint:
-        printData( resp )
-    return resp
+    printData( Hive( man, args.hive_name, altPartitionKey = args.partitionKey ).get( args.key ).toJSON() )
 
-def do_get_mtd( args, man, isPrint = True ):
+def _do_get_mtd( args, man ):
     if args.key is None:
         reportError( 'Key required' )
 
-    resp = man._apiCall( 'hive/%s/%s/%s/mtd' % ( args.hive_name, args.partitionKey, urlescape( args.key ) ), GET )
-    if isPrint:
-        printData( resp )
-    return resp
+    printData( Hive( man, args.hive_name, altPartitionKey = args.partitionKey ).getMetadata( args.key ).toJSON() )
 
-def do_add( args, man, isPrint = True ):
+def _do_add( args, man ):
     if args.key is None:
         reportError( 'Key required' )
 
-    target = 'mtd'
+    record = {
+        'data' : None,
+    }
 
-    data = None
     if args.data is not None:
         data = open( args.data, 'rb' ).read().decode()
         data = json.loads( data )
-        target = 'data'
+        record[ 'data' ] = data
 
     usrMtd = {}
     if args.expiry is not None:
@@ -163,74 +183,38 @@ def do_add( args, man, isPrint = True ):
         usrMtd[ 'enabled' ] = args.enabled.lower() not in ( '0', 'false', 'no', 'off' )
     if args.tags is not None:
         usrMtd[ 'tags' ] = [ t.strip() for t in args.tags.split( ',' ) ]
+    record[ 'usr_mtd' ] = usrMtd
 
-    req = {
-        'data' : json.dumps( data ),
-    }
-
+    sysMtd = {}
     if args.etag is not None:
-        req[ 'etag' ] = args.etag
-    if len( usrMtd ) != 0:
-        req[ 'usr_mtd' ] = json.dumps( usrMtd )
+        sysMtd[ 'etag' ] = args.etag
+    record[ 'sys_mtd' ] = sysMtd
 
-    resp = man._apiCall( 'hive/%s/%s/%s/%s' % ( args.hive_name, args.partitionKey, urlescape( args.key ), target ), POST, req )
-    if isPrint:
-        printData( resp )
-    return resp
+    printData( Hive( man, args.hive_name, altPartitionKey = args.partitionKey ).set( HiveRecord( args.key, record ) ) )
 
-def do_update( args, man, isPrint = True ):
+def _do_update( args, man ):
     if args.key is None:
         reportError( 'Key required' )
 
-    target = 'mtd'
+    _do_add( args, man )
 
-    data = None
-    if args.data is not None:
-        data = open( args.data, 'rb' ).read().decode()
-        data = json.loads( data )
-        target = 'data'
-        existing = do_get( args, man, isPrint = False )
-        existing[ 'data' ] = json.dumps( data )
-    else:
-        existing = do_get_mtd( args, man, isPrint = False )
-        existing.pop( 'data', None )
-        existing[ 'etag' ] = existing[ 'sys_mtd' ].pop( 'etag' )
-        existing.pop( 'sys_mtd', None )
-
-    if args.expiry is not None:
-        existing[ 'usr_mtd' ][ 'expiry' ] = args.expiry
-    if args.enabled is not None:
-        existing[ 'usr_mtd' ][ 'enabled' ] = args.enabled.lower() not in ( '0', 'false', 'no', 'off' )
-    if args.tags is not None:
-        existing[ 'usr_mtd' ][ 'tags' ] = [ t.strip() for t in args.tags.split( ',' ) ]
-
-    existing[ 'usr_mtd' ] = json.dumps( existing[ 'usr_mtd' ] )
-
-    resp = man._apiCall( 'hive/%s/%s/%s/%s' % ( args.hive_name, args.partitionKey, urlescape( args.key ), target ), POST, existing )
-    if isPrint:
-        printData( resp )
-    return resp
-
-def do_remove( args, man, isPrint = True ):
+def _do_remove( args, man ):
     if args.key is None:
         reportError( 'Key required' )
 
-    resp = man._apiCall( 'hive/%s/%s/%s' % ( args.hive_name, args.partitionKey, urlescape( args.key ) ), DELETE )
-    if isPrint:
-        printData( resp )
-    return resp
+    printData( Hive( man, args.hive_name, altPartitionKey = args.partitionKey ).delete( args.key ) )
 
 def main( sourceArgs = None ):
     import argparse
 
     actions = {
-        'list' : do_list,
-        'list_mtd' : do_list_mtd,
-        'get' : do_get,
-        'get_mtd' : do_get_mtd,
-        'set' : do_add,
-        'update' : do_update,
-        'remove' : do_remove,
+        'list' : _do_list,
+        'list_mtd' : _do_list_mtd,
+        'get' : _do_get,
+        'get_mtd' : _do_get_mtd,
+        'set' : _do_add,
+        'update' : _do_update,
+        'remove' : _do_remove,
     }
 
     parser = argparse.ArgumentParser( prog = 'limacharlie hive' )
