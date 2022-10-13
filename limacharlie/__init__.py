f4ab926573afe9f50175c7014b982573e4ff3f71
Maxime Lamothe-Brassard <maxime@refractionpoint.com>
2022-10-13 13:11:16 -0500

    Conf tweaks (#65)
    * Fix handling of backoff from infra service.
    
    * Bump version.
    
    * Python 2 compat.

--- limacharlie/__init__.py
+++ limacharlie/__init__.py
@@ -1,6 +1,6 @@
 """limacharlie API for limacharlie.io"""
 
-__version__ = "4.1.3"
+__version__ = "4.1.4"
 __author__ = "Maxime Lamothe-Brassard ( Refraction Point, Inc )"
 __author_email__ = "maxime@refractionpoint.com"
 __license__ = "Apache v2"
