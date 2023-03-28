ae2dd8d4225584192c8c3f7b6d169677f47024ea
Maxime Lamothe-Brassard <maxime@refractionpoint.com>
2023-03-28 13:06:48 -0700

    More draft access to extensions.

--- limacharlie/Extensions.py
+++ limacharlie/Extensions.py
@@ -5,7 +5,9 @@ import json
 import yaml
 
 from .utils import POST
+from .utils import DELETE
 from .utils import GET
+from .utils import PUT
 
 class Extension( object ):
     def __init__( self, manager ):
@@ -22,6 +24,30 @@ class Extension( object ):
             'action' : action,
             'data' : json.dumps( data ),
         } )
+    
+    def list( self, extName ):
+        return self._manager._apiCall( 'orgs/%s/subscription' % ( self._manager._oid, ), GET )
 
+    def subscribe( self, extName ):
+        return self._manager._apiCall( 'orgs/%s/subscription/extension/%s' % ( self._manager._oid, extName, ), POST, {} )
+
+    def unsubscribe( self, extName ):
+        return self._manager._apiCall( 'orgs/%s/subscription/extension/%s' % ( self._manager._oid, extName, ), DELETE, {} )
+    
+    def getAll( self, extName ):
+        return self._manager._apiCall( 'extension/definition', GET, {} )
+
+    def create( self, extObj ):
+        return self._manager._apiCall( 'extension/definition', POST, extObj )
+    
+    def update( self, extName, extObj ):
+        return self._manager._apiCall( 'extension/definition', PUT, extObj )
+    
+    def get( self, extName ):
+        return self._manager._apiCall( 'extension/definition/%s' % ( extName, ), GET )
+    
+    def delete( self, extName ):
+        return self._manager._apiCall( 'extension/definition/%s' % ( extName, ), DELETE )
+    
     def getSchema( self, extName ):
-        return self._manager._apiCall( 'extension/schema/%s' % ( extName, ), GET, {} )
+        return self._manager._apiCall( 'extension/schema/%s' % ( extName, ), GET )
