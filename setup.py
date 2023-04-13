392d192ead1f53ba3fbd288371c9f02691b8f10f
Maxime Lamothe-Brassard <maxime@refractionpoint.com>
2023-04-13 11:23:35 -0700

    Adding an env switch to extensions.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-__version__ = "4.4.2"
+__version__ = "4.4.3"
 __author__ = "Maxime Lamothe-Brassard ( Refraction Point, Inc )"
 __author_email__ = "maxime@refractionpoint.com"
 __license__ = "Apache v2"
