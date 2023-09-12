0480d378415663f1ac9e9e32c4345b3418246332
Maxime Lamothe-Brassard <maxime@refractionpoint.com>
2023-09-12 15:46:51 -0700

    Missing hives in sync. (#110)
    * Missing hives in sync.
    
    * Missing cli

--- limacharlie/Configs.py
+++ limacharlie/Configs.py
@@ -960,6 +960,18 @@ def main( sourceArgs = None ):
                          action = 'store_true',
                          dest = 'isHiveYara',
                          help = 'if specified, apply yara rules in hive from operations' )
+    parser.add_argument( '--hive-lookup',
+                         required = False,
+                         default = False,
+                         action = 'store_true',
+                         dest = 'isHiveLookup',
+                         help = 'if specified, apply lookups in hive from operations' )
+    parser.add_argument( '--hive-secret',
+                         required = False,
+                         default = False,
+                         action = 'store_true',
+                         dest = 'isHiveSecret',
+                         help = 'if specified, apply secrets in hive from operations' )
     parser.add_argument( '--all',
                          required = False,
                          default = False,
@@ -1018,6 +1030,8 @@ def main( sourceArgs = None ):
         'isHiveCloudSensor',
         'isHiveExtensionConfig',
         'isHiveYara',
+        'isHiveLookup',
+        'isHiveSecret',
     ]
 
     allHives = {
@@ -1028,6 +1042,8 @@ def main( sourceArgs = None ):
         'cloud_sensor': True,
         'extension_config': True,
         'yara': True,
+        'lookup': True,
+        'secret': True,
     }
 
     # If All is enabled, enable all types.
@@ -1060,6 +1076,10 @@ def main( sourceArgs = None ):
         hives['extension_config'] = True
     if args.isHiveYara:
         hives['yara'] = True
+    if args.isHiveLookup:
+        hives['lookup'] = True
+    if args.isHiveSecret:
+        hives['secret'] = True
 
     s = Configs( oid = args.oid, env = args.environment, isDontUseInfraService = args.isDontUseInfraService, isUseExtension = args.isUseExtension )
 
