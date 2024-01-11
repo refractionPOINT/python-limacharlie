9476bdccb97c1bb5e23a5af5744c5831142ccbff
josh-lc <136113933+josh-lc@users.noreply.github.com>
2024-01-11 14:05:07 -0600

    adding webook sender to init file and update version (#119)

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-__version__ = "4.7.0"
+__version__ = "4.7.1"
 __author__ = "Maxime Lamothe-Brassard ( Refraction Point, Inc )"
 __author_email__ = "maxime@refractionpoint.com"
 __license__ = "Apache v2"
