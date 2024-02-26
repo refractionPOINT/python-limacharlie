0b0e3abe46a8eab7f1ed56c1137a1dce1a24bdcc
Adele Noronha <adele@refractionpoint.com>
2024-02-26 15:03:28 -0800

    add ext-pagerduty + code tidy

--- limacharlie/Extensions.py
+++ limacharlie/Extensions.py
@@ -13,123 +13,6 @@ class Extension( object ):
     def __init__( self, manager ):
         self._manager = manager
 
-    def convert_rules(self, extName, isDryRun = True): 
-        updated_rules = []
-        # hive get all rules
-        hive = Hive.Hive(self._manager, "dr-general")
-        gen_dr_rules = hive.list()
-        for rule_name in gen_dr_rules:
-            hive_record = hive.get(rule_name)
-            dnr = getattr(hive_record, 'data', None)
-            if dnr is not None:
-                stringDnr = str(dnr)
-                if extName == 'ext-zeek' and "'action': 'service request', 'name': 'zeek'" in stringDnr:
-                    detect = dnr.get('detect', None)
-                    resp_items = dnr.get('respond', None)
-                    updated_respond = []
-                    if resp_items is not None and detect is not None:                    
-                        # filter respond block, check "action: service request"                
-                        for resp_item in resp_items:
-                                if resp_item['action'] == 'service request':                                     
-                                    # check if org actually has ext installed // send out error if no [Max said to leave this till later]
-                                    request = resp_item['request']
-                                    art_id = '{{ .routing.sid }}' if request['artifact_id'] == '<<routing/log_id>>' else make_transform_exp(request['artifact_id'])
-                                    ext_resp = {
-                                            "action": "extension request",
-                                            "extension action": "run_on",
-                                            "extension name": "ext-zeek",
-                                            "extension request": {
-                                                "artifact_id": art_id,
-                                                "retention": request['retention']
-                                            },
-                                        }
-                                    updated_respond.append(ext_resp)
-                                else :
-                                    updated_respond.append(resp_item)
-                    new_dnr = {
-                                "detect": detect,
-                                "respond": updated_respond,
-                        }
-                    zeek_changed_rule = {
-                        'r_name': rule_name,
-                        'old_dnr': json.dumps(dnr, indent=2),
-                        'new_dnr': json.dumps(new_dnr, indent=2),
-                    }
-                    updated_rules.append(zeek_changed_rule)  
-                        
-                # stringDnr = str(dnr)
-                # if "'action': 'service request'," in stringDnr:
-                #     print(stringDnr)
-                if "'action': 'service request', 'name': 'atomic-red-team'" in stringDnr:
-                    print(f"Atomic Red Team rule: {stringDnr}")
-                if "'action': 'service request', 'name': 'dumper'" in stringDnr:
-                    print(f"Dumper rule: {stringDnr}")
-                if "'action': 'service request', 'name': 'exfil'" in stringDnr:
-                    print(f"Exfil rule: {stringDnr}")
-                if "'action': 'service request', 'name': 'integrity'" in stringDnr:
-                    print(f"Integrity rule: {stringDnr}")
-                if "'action': 'service request', 'name': 'pager-duty'" in stringDnr:
-                    print(f"Pager-Duty rule: {stringDnr}")
-                if "'action': 'service request', 'name': 'reliable-tasking'" in stringDnr:
-                    print(f"Reliable-tasking rule: {stringDnr}")
-                if "'action': 'service request', 'name': 'sensor-cull'" in stringDnr:
-                    print(f"Sensor-cull rule: {stringDnr}")
-                if "'action': 'service request', 'name': 'snapattack'" in stringDnr:
-                    print(f"Snapattack rule: {stringDnr}")
-                if "'action': 'service request', 'name': 'velociraptor'" in stringDnr:
-                    print(f"Velociraptor rule: {stringDnr}")
-                if "'action': 'service request', 'name': 'yara'" in stringDnr:
-                    print(f"Yara rule: {stringDnr}")
-                # if "'action': 'service request', 'name': 'zeek'" in stringDnr:
-                    # new_dnr_string = stringDnr.replace(
-                    #     "'action': 'service request', 'name': 'zeek', 'request': {'action': 'run_on', ", 
-                    #     "'action': 'extension request', 'extension action': 'run_on', 'extension name': 'ext-zeek', 'extension request': {"
-                    #     )
-                    # zeek_changed_rule = {
-                    #     'r_name': rule_name,
-                    #     'old_dnr': stringDnr,
-                    #     'new_dnr': new_dnr_string,
-                    # }
-                    # updated_rules.append(zeek_changed_rule)
-
-        #  if isDryRun, don't send request, print changes
-        if isDryRun and len(updated_rules) > 0:
-            for updated_rule in updated_rules:
-                print(f"Dry run of change on rule '{updated_rule['r_name']}':")
-                print("\033[91m- {}\033[0m".format(updated_rule['old_dnr']))
-                print("\033[92m+ {}\033[0m".format(updated_rule['new_dnr']))
-        if not isDryRun and len(updated_rules) > 0:
-            for updated_rule in updated_rules:
-                data = {
-                    "data": updated_rule['new_dnr']
-                }
-                # hive change rule
-                try:
-                    hr = Hive.HiveRecord(updated_rule['r_name'], data)
-                    hive.set(hr)
-                except Exception as e:
-                    raise LcApiException(f"failed to create detect response for run : {e}")
-      
-        print("end of func")
-        return  
-    
-    def migrate( self, extName ):
-        return self._manager._apiCall( 'extension/migrate/%s' % ( extName, ), POST, {
-            'oid' : self._manager._oid,
-        } )
-
-    def request( self, extName, action, data = {}, isImpersonated = False ):
-        req = {
-            'oid' : self._manager._oid,
-            'action' : action,
-            'data' : json.dumps( data ),
-        }
-        if isImpersonated:
-            if self._manager._jwt is None:
-                self._manager._refreshJWT()
-            req[ 'impersonator_jwt' ] = self._manager._jwt
-        return self._manager._apiCall( 'extension/request/%s' % ( extName, ), POST, req )
-    
     def list( self ):
         return self._manager._apiCall( 'orgs/%s/subscriptions' % ( self._manager._oid, ), GET )
 
@@ -159,7 +42,73 @@ class Extension( object ):
     
     def getSchema( self, extName ):
         return self._manager._apiCall( 'extension/schema/%s' % ( extName, ), GET )
+    
+    def migrate( self, extName ):
+        return self._manager._apiCall( 'extension/migrate/%s' % ( extName, ), POST, {
+            'oid' : self._manager._oid,
+        } )
 
+    def request( self, extName, action, data = {}, isImpersonated = False ):
+        req = {
+            'oid' : self._manager._oid,
+            'action' : action,
+            'data' : json.dumps( data ),
+        }
+        if isImpersonated:
+            if self._manager._jwt is None:
+                self._manager._refreshJWT()
+            req[ 'impersonator_jwt' ] = self._manager._jwt
+        return self._manager._apiCall( 'extension/request/%s' % ( extName, ), POST, req )
+    
+    def convert_rules(self, extName, isDryRun = True): 
+        updated_rules = []
+        # hive get all rules
+        hive = Hive.Hive(self._manager, "dr-general")
+        gen_dr_rules = hive.list()
+        for rule_name in gen_dr_rules:
+            hive_record = hive.get(rule_name)
+            dnr = getattr(hive_record, 'data', None)
+            if dnr is not None:
+                detect = dnr.get('detect', None)
+                resp_items = dnr.get('respond', None)  
+                if resp_items is not None and detect is not None:
+                    if extName == 'ext-zeek' and contains_action_name(resp_items, 'zeek'):  
+                        new_zeek_dnr = update_rule(detect, resp_items, extName)
+                        zeek_rule_data = {
+                            'r_name': rule_name,
+                            'old_dnr': json.dumps(dnr, indent=2),
+                            'new_dnr': json.dumps(new_zeek_dnr, indent=2),
+                        }
+                        updated_rules.append(zeek_rule_data)  
+                    if extName == 'ext-pagerduty' and contains_action_name(resp_items, 'pagerduty'):
+                        new_pagerduty_dnr = update_rule(detect, resp_items, extName)
+                        pagerduty_changed_rule = {
+                            'r_name': rule_name,
+                            'old_dnr': json.dumps(dnr, indent=2),
+                            'new_dnr': json.dumps(new_pagerduty_dnr, indent=2),
+                        }
+                        updated_rules.append(pagerduty_changed_rule)     
+        #  if isDryRun, don't send request, print changes
+        if isDryRun and len(updated_rules) > 0:
+            for updated_rule in updated_rules:
+                print(f"Dry run of change on rule '{updated_rule['r_name']}':")
+                print("\033[91m- {}\033[0m".format(updated_rule['old_dnr']))
+                print("\033[92m+ {}\033[0m".format(updated_rule['new_dnr']))
+        if not isDryRun and len(updated_rules) > 0:
+            for updated_rule in updated_rules:
+                data = {
+                    "data": updated_rule['new_dnr']
+                }
+                # hive change rule
+                try:
+                    hr = Hive.HiveRecord(updated_rule['r_name'], data)
+                    hive.set(hr)
+                except Exception as e:
+                    raise LcApiException(f"failed to create detect response for run : {e}")
+      
+        print("end of func")
+        return  
+    
 def printData( data ):
     if isinstance( data, str ):
         print( data )
@@ -262,5 +211,63 @@ if '__main__' == __name__:
 
 
 
+### Convert Rules Helper Functions
+def update_rule(detect, respond_items, extName):
+    print('EXT name in update_rule', extName)
+    # check if org actually has ext installed // send out error if no [Max said to leave this till later]
+    updated_respond = []
+    for resp_item in respond_items:
+        if resp_item['action'] == 'service request':
+            request = resp_item['request']
+            ext_resp = convert_response(request, extName)                                     
+            updated_respond.append(ext_resp)
+        else :
+            updated_respond.append(resp_item)
+    new_dnr = {
+        "detect": detect,
+        "respond": updated_respond,
+    }
+    return new_dnr
+
+def convert_response(req, extName):
+    if extName == "ext-zeek":
+        art_id = '{{ .routing.sid }}' if req['artifact_id'] == '<<routing/log_id>>' else make_transform_exp(req['artifact_id'])
+        ext_zeek_resp = {
+            "action": "extension request",
+            "extension action": "run_on",
+            "extension name": "ext-zeek",
+            "extension request": {
+                "artifact_id": art_id,
+                "retention": req['retention']
+            },
+        }
+        return ext_zeek_resp
+    elif extName == 'ext-pagerduty':
+        ext_pagerduty_resp = {
+            "action": "extension request",
+            "extension action": "run",
+            "extension name": "ext-pagerduty",
+            "extension request": {
+                "class": make_transform_exp(req['class']),
+                "group": make_transform_exp(req['group']),
+                "severity": make_transform_exp(req['severity']),
+                "source": make_transform_exp(req['source']),
+                "component": make_transform_exp(req['component']),
+                "summary": req['summary'],
+                "details": '{{ .event }}',
+            }
+        }
+        return ext_pagerduty_resp
+    elif extName == 'atomic-red-team':
+        return {}
+    else:
+        return {}
+    
+def contains_action_name(respond_items, svc_name):
+    for resp_item in respond_items:
+        if resp_item['action'] == 'service request' and resp_item['name'] == svc_name:
+            return True
+    return False
+ 
 def make_transform_exp(str_var):
     return  '{{' + ' "' + str(str_var) + '" ' + '}}' 
