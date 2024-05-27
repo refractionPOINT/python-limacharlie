db6c1f7e4d26f57fd8c10c3c9b105bc3884f12e0
dzimine-lc <dmitri@limacharlie.io>
2024-05-27 17:38:43 -0500

    Suppress stack trace print in CLI

--- limacharlie/__main__.py
+++ limacharlie/__main__.py
@@ -4,7 +4,7 @@ _IS_PYTHON_2 = False
 if sys.version_info[ 0 ] < 3:
     _IS_PYTHON_2 = True
 
-def main():
+def cli():
     import argparse
     import getpass
     import uuid
@@ -380,6 +380,13 @@ def main():
         print( json.dumps( _man.getSensorsWithIp( args.ip, start, end ), indent = 2 ) )
     else:
         raise Exception( 'invalid action' )
+    
+def main():
+    try:
+        cli()
+    except Exception as e:
+        print("Error:", e)
 
 if __name__ == "__main__":
     main()
+ 
