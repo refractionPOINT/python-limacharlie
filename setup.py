bda6965d00f9510c83ee4b625462cee7b6c24724
Maxime Lamothe-Brassard <maxime@refractionpoint.com>
2024-07-26 21:01:17 -0700

    Remove ARL from dependencies. (#134)

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup( name = 'limacharlie',
        license = __license__,
        packages = [ 'limacharlie' ],
        zip_safe = True,
-       install_requires = [ 'requests', 'passlib', 'pyyaml', 'tabulate', 'termcolor', 'arl' ],
+       install_requires = [ 'requests', 'passlib', 'pyyaml', 'tabulate', 'termcolor' ],
        long_description = 'Python API for limacharlie.io, an endpoint detection and response service.',
        entry_points = {
            'console_scripts': [
