30ab2455c847e86da9540e21e5a33ca7b0d2572f
Joshua Padilla <joshua@refractionpoint.com>
2024-09-25 15:29:31 -0500

    updating version number

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-__version__ = "4.9.5"
+__version__ = "4.9.6"
 __author__ = "Maxime Lamothe-Brassard ( Refraction Point, Inc )"
 __author_email__ = "maxime@refractionpoint.com"
 __license__ = "Apache v2"
